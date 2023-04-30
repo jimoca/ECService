08ff56cfafef00dcbc5636fb2b279566625a6310
Jim <jinchglin@gmail.com>
2023-04-30 13:47:32 +0800

    optimization

--- src/main/java/com/ec/ecservice/utils/MaskWithPaillier.java
+++ src/main/java/com/ec/ecservice/utils/MaskWithPaillier.java
@@ -7,6 +7,8 @@ import security.paillier.PaillierCipher;
 import security.paillier.PaillierPublicKey;
 
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MaskWithPaillier {
     public static BigInteger[] apply(PictureModel image, PictureModel mask, PaillierPublicKey pk) {
@@ -19,19 +21,26 @@ public class MaskWithPaillier {
         BigInteger[] big_img_pixels = image.getPixels();
         BigInteger[] big_mask_pixels = mask.getPixels();
 
-        int img_start_row = img_height/2 - mask_height/2;
-        int img_start_col = img_width/2 - mask_width/2;
+        int img_start_row = img_height / 2 - mask_height / 2;
+        int img_start_col = img_width / 2 - mask_width / 2;
+
+        Map<BigInteger, BigInteger> multiplyMap = new HashMap<>();
 
         System.out.println("operating");
 
         try {
             for (int i = 0; i < mask_height; i++) {
                 int img_index = (img_width * img_start_row) + img_start_col;
-                if(img_start_row >= img_height || img_index >= big_img_pixels.length) {
+                if (img_start_row >= img_height || img_index >= big_img_pixels.length) {
                     break;
                 }
                 for (int j = i * mask_width; j < i * mask_width + mask_width; j++) {
-                    big_img_pixels[img_index] = PaillierCipher.add(big_img_pixels[img_index], PaillierCipher.multiply(big_mask_pixels[j], 16777216, pk), pk);
+                    if (!multiplyMap.containsKey(big_mask_pixels[j])) {
+                        BigInteger resOfMultiply = PaillierCipher.multiply(big_mask_pixels[j], 16777216, pk);
+                        multiplyMap.put(big_mask_pixels[j], resOfMultiply);
+                    }
+
+                    big_img_pixels[img_index] = PaillierCipher.add(big_img_pixels[img_index], multiplyMap.get(big_mask_pixels[j]), pk);
                     img_index++;
                 }
                 img_start_row++;
